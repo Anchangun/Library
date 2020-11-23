af2acad5717584a12e5079f1503395b9e379e55d
DESKTOP-BVI2QKE\wavem <changun516@gmail.com>
2020-11-23 14:05:41 +0900

    DegreeConvert Insert

---
+++ Cpp/Math/DegreeConvert/DegreeConvert.cpp
@@ -0,0 +1,8 @@
+#include "DegreeConvert.h"
+
+double DegreeConvert::degreeToRadian(double degree){
+
+	std::vector<std::string>::iterator iter;
+
+	return degree * M_PI / 180;
+}
---
+++ Cpp/Math/DegreeConvert/DegreeConvert.h
@@ -0,0 +1,12 @@
+#pragma once
+
+#define _USE_MATH_DEFINES
+#include<math.h>
+#include<iostream>
+#include <vector>
+class DegreeConvert
+{
+public : 
+	double degreeToRadian(double degree);
+};
+
