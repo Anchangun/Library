522d5d8c0fcabd30719eec145913f6be59766005
DESKTOP-V7DNS3G\chang <changun516@gmail.com>
2020-12-01 00:36:32 +0900

    STRING TO INT 추가

--- Cpp/Msg/DataTypeConvert/DataTypeConvert.cpp
+++ Cpp/Msg/DataTypeConvert/DataTypeConvert.cpp
@@ -1,7 +1,12 @@
 #include"StringMsg/DataTypeConvert.h"
-
 float DataTypeConvert::stringToFloat(std::string string_data){
 	float temp_float;
 	temp_float=std::stof(string_data);
 	return temp_float;
+}
+
+int DataTypeConvert::stringToInt(std::string string_data)
+{
+	int convert = std::stoi(string_data);
+	return convert;
 }
--- Cpp/Msg/DataTypeConvert/DataTypeConvert.h
+++ Cpp/Msg/DataTypeConvert/DataTypeConvert.h
@@ -1,7 +1,8 @@
 #include<iostream>
-
+#include<string>
 class DataTypeConvert{
 
 	public : 
-		float stringToFloat(std::string);
+		float stringToFloat(std::string string_data);
+		int stringToInt(std::string string_data);
 };
