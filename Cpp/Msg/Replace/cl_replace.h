aeefee110da6a7db72bbdf2ca89e4ef2f6b60424
DESKTOP-BVI2QKE\wavem <changun516@gmail.com>
2021-09-03 14:30:28 +0900

    Create : type : class replace all

---
+++ Cpp/Msg/Replace/cl_replace.h
@@ -0,0 +1,8 @@
+#pragma once
+#include<iostream>
+class cl_replace
+{
+public :
+	std::string fn_replace_all(const std::string& str, const std::string& target_str, const std::string& convert_str);
+};
+
